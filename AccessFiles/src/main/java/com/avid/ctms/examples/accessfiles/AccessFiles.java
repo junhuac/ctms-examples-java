8dade6f8104dfaac437e430f77449666169b0053
Junhua Chang <jchang@sbgtv.com>
2018-07-26 18:18:24 -0700

    Fix Invalid cookie header warning.

--- AccessFiles/src/main/java/com/avid/ctms/examples/accessfiles/AccessFiles.java
+++ AccessFiles/src/main/java/com/avid/ctms/examples/accessfiles/AccessFiles.java
@@ -5,6 +5,9 @@ import com.avid.ctms.examples.tools.unirest.PlatformToolsUnirest;
 import com.damnhandy.uri.template.*;
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import org.json.*;
 
 import java.net.*;
@@ -29,26 +32,37 @@ public class AccessFiles {
     private AccessFiles() {
     }
 
-    private static void getFileInformation(String urlAccessFileByUsageAndProtocol) throws UnirestException {
-        final HttpResponse<JsonNode> filesResponse =
-                Unirest.get(urlAccessFileByUsageAndProtocol)
-                        .header("Accept", "application/hal+json")
-                        .asJson();
-        final int filesStatus = filesResponse.getStatus();
-        if (HttpURLConnection.HTTP_OK == filesStatus) {
-            final JSONArray files = filesResponse.getBody().getObject().getJSONObject("_embedded").getJSONArray("aa:access-file");
-            final StringBuilder sb = new StringBuilder(String.format("Requested: <%s>%n", urlAccessFileByUsageAndProtocol));
-            try (final Formatter formatter = new Formatter(sb)) {
-                for (int i = 0; i < files.length(); ++i) {
-                    final JSONObject file = files.getJSONObject(i);
-                    formatter.format("\t%s;%s%n", file.get("protocol"), file.getJSONObject("_links").getJSONObject("ma:essence").get("href"));
-                }
-            }
+    private static void getFileInformation(String urlAccessFileByUsageAndProtocol) throws Exception {
 
-            LOG.log(Level.INFO, sb::toString);
-        } else {
-            LOG.log(Level.INFO, "Error getting resource <{0}>. -> {1}", new Object[] {urlAccessFileByUsageAndProtocol, filesResponse.getStatusText()});
-        }
+        try {
+            URL simpleSearchResultPageURL = new URL(urlAccessFileByUsageAndProtocol);
+            final HttpURLConnection simpleSearchResultPageConnection = (HttpURLConnection) simpleSearchResultPageURL.openConnection();
+            simpleSearchResultPageConnection.setConnectTimeout(PlatformTools.getDefaultConnectionTimeoutms());
+            simpleSearchResultPageConnection.setReadTimeout(PlatformTools.getDefaultReadTimeoutms());
+            simpleSearchResultPageConnection.setRequestProperty("Accept", "application/hal+json");
+
+            final int simpleSearchStatus = simpleSearchResultPageConnection.getResponseCode();
+            if (HttpURLConnection.HTTP_OK == simpleSearchStatus) {
+                final String rawSimpleSearchPageResult = PlatformTools.getContent(simpleSearchResultPageConnection);
+
+                final JSONObject searchesResult = JSONObject.fromObject(rawSimpleSearchPageResult);
+                final JSONArray files = searchesResult.getJSONObject("_embedded").getJSONArray("aa:access-file");
+                final StringBuilder sb = new StringBuilder(String.format("Requested: <%s>%n", urlAccessFileByUsageAndProtocol));
+                try (final Formatter formatter = new Formatter(sb)) {
+                    for (int i = 0; i < files.size(); ++i) {
+                        final JSONObject file = files.getJSONObject(i);
+                        formatter.format("\t%s;%s%n", file.get("protocol"), file.getJSONObject("_links").getJSONObject("ma:essence").get("href"));
+                    }
+                }
+
+                LOG.log(Level.INFO, sb::toString);
+            } else {
+                LOG.log(Level.INFO, "Error getting resource <{0}>. -> {1}", new Object[] {urlAccessFileByUsageAndProtocol, simpleSearchResultPageConnection.getResponseMessage()});
+            }
+        } catch (final Exception exception) {
+                LOG.log(Level.SEVERE, "failure", exception);
+            } finally {
+            }
     }
 
     public static void main(String[] args) throws Exception {
@@ -64,44 +78,51 @@ public class AccessFiles {
             // deutsch.mp3: 2016051315594660101291561460050569B02260000003692B00000D0D000003
             final String assetID = args[6];
 
-            final boolean successfullyAuthorized = PlatformToolsUnirest.authorize(apiDomain, username, password);
+            final boolean successfullyAuthorized = PlatformTools.authorize(apiDomain, username, password);
             if(successfullyAuthorized) {
                 try {
                     /// Query CTMS Registry:
                     final String registryServiceVersion = "0";
                     final String defaultAssetByIDUriTemplate = String.format("https://%s/apis/%s;version=%s;realm=%s/assets/{id}", apiDomain, serviceType, serviceVersion, realm);
-                    final List<String> assetByIDUriTemplates = PlatformToolsUnirest.findInRegistry(apiDomain, Collections.singletonList(serviceType), registryServiceVersion, "aa:asset-by-id", defaultAssetByIDUriTemplate);
+                    final List<String> assetByIDUriTemplates = PlatformTools.findInRegistry(apiDomain, Collections.singletonList(serviceType), registryServiceVersion, "aa:asset-by-id", defaultAssetByIDUriTemplate);
 
                     /// Prepare simple search request:
                     final UriTemplate searchURITemplate = UriTemplate.fromTemplate(assetByIDUriTemplates.get(0));
                     URL simpleSearchResultPageURL = new URL(searchURITemplate.set("id", assetID).expand());
 
-                    /// Directly getFileInformation the asset:
-                    final HttpResponse<JsonNode> assetResponse =
-                        Unirest.get(simpleSearchResultPageURL.toString())
-                                .header("Accept", "application/hal+json")
-                                .asJson();
+                    final HttpURLConnection simpleSearchResultPageConnection = (HttpURLConnection) simpleSearchResultPageURL.openConnection();
+                    simpleSearchResultPageConnection.setConnectTimeout(PlatformTools.getDefaultConnectionTimeoutms());
+                    simpleSearchResultPageConnection.setReadTimeout(PlatformTools.getDefaultReadTimeoutms());
+                    simpleSearchResultPageConnection.setRequestProperty("Accept", "application/hal+json");
 
-                    final int assetStatus = assetResponse.getStatus();
-                    if (HttpURLConnection.HTTP_OK == assetStatus) {
-                        String urlAccessFileByUsageAndProtocol = assetResponse.getBody().getObject().getJSONObject("_links").getJSONObject("aa:access-file-by-type-usage-and-protocol").getString("href");
-                        urlAccessFileByUsageAndProtocol  = urlAccessFileByUsageAndProtocol.substring(0, urlAccessFileByUsageAndProtocol.lastIndexOf('{'));
+                    final int simpleSearchStatus = simpleSearchResultPageConnection.getResponseCode();
+                    if (HttpURLConnection.HTTP_OK == simpleSearchStatus) {
+                        final String rawSimpleSearchPageResult = PlatformTools.getContent(simpleSearchResultPageConnection);
 
-                        // Get all files:
-                        getFileInformation(urlAccessFileByUsageAndProtocol);
-                        // Get files with usage=BROWSE:
-                        getFileInformation(urlAccessFileByUsageAndProtocol + "?usage=BROWSE");
-                        // Get files with filetype=VIDEO and usage=BROWSE:
-                        getFileInformation(urlAccessFileByUsageAndProtocol + "?filetype=VIDEO&usage=BROWSE");
-                        // Get files with filetype=FSET:
-                        getFileInformation(urlAccessFileByUsageAndProtocol + "?filetype=FSET");
+                        final JSONObject searchesResult = JSONObject.fromObject(rawSimpleSearchPageResult);
+                        final JSONObject advancedSearchLinkObject = searchesResult.getJSONObject("_links").getJSONObject("aa:access-file-by-type-usage-and-protocol");
+                        // Is advanced search supported?
+                        if (null != advancedSearchLinkObject) {
+                            /// Doing the advanced search and write the results to stdout:
+                            String urlAccessFileByUsageAndProtocol = advancedSearchLinkObject.getString("href");
+                            urlAccessFileByUsageAndProtocol = urlAccessFileByUsageAndProtocol.substring(0, urlAccessFileByUsageAndProtocol.lastIndexOf('{'));
+
+                            // Get all files:
+                            getFileInformation(urlAccessFileByUsageAndProtocol);
+                            // Get files with usage=BROWSE:
+                            getFileInformation(urlAccessFileByUsageAndProtocol + "?usage=BROWSE");
+                            // Get files with filetype=VIDEO and usage=BROWSE:
+                            getFileInformation(urlAccessFileByUsageAndProtocol + "?filetype=VIDEO&usage=BROWSE");
+                            // Get files with filetype=FSET:
+                            getFileInformation(urlAccessFileByUsageAndProtocol + "?filetype=FSET");
+                        }
                     } else {
-                        LOG.log(Level.INFO, "Error getting resource <{0}>. -> {1}", new Object[] {simpleSearchResultPageURL, assetResponse.getStatusText()});
+                            LOG.log(Level.INFO, "Error getting resource <{0}>. -> {1}", new Object[]{simpleSearchResultPageURL, simpleSearchResultPageConnection.getResponseMessage()});
                     }
                 } catch (final Exception exception) {
                     LOG.log(Level.SEVERE, "failure", exception);
                 } finally {
-                    PlatformToolsUnirest.logout(apiDomain);
+                    PlatformTools.logout(apiDomain);
                 }
             } else {
                 LOG.log(Level.INFO, "Authorization failed.");
